26215bb27122ee8b30fad8fdfecc7f4d1fc622e0
Laura Miller <lauramiller.spence@gmail.com>
2020-05-12 16:22:22 +0100

    Tidied code

--- reversal2.py
+++ reversal2.py
@@ -1,13 +1,10 @@
 string = input("Enter text to be reversed: " )
 
 new_string = (string[::-1]) # reverses the entire string
-print(new_string)
+# print(new_string)
 
 x = new_string.split()
-print(x)
+# print(x)
 
 reversed = ' '.join(reversed(x))
 print(reversed)
-
-
-# print(x[::-1])
