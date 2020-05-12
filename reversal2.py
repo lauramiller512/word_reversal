c0529643987b8ff7dd2409f965dc9354b4b63381
Laura Miller <lauramiller.spence@gmail.com>
2020-05-12 16:13:43 +0100

    Function currently splits each word and reverses the letters

---
+++ reversal2.py
@@ -0,0 +1,11 @@
+string = input("Enter text to be reversed: " )
+
+new_string = (string[::-1]) # reverses the entire string
+print(new_string)
+x = new_string.split()
+print(x)
+
+
+
+
+# print(x[::-1])
