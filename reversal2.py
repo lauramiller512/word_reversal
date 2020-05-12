429654555a549f6ebed09b1164fe6ad4c595c776
Laura Miller <lauramiller.spence@gmail.com>
2020-05-12 16:19:33 +0100

    Function now reverses letters within a word and keeps the words in the same place, there's no space between words though

--- reversal2.py
+++ reversal2.py
@@ -2,10 +2,12 @@ string = input("Enter text to be reversed: " )
 
 new_string = (string[::-1]) # reverses the entire string
 print(new_string)
+
 x = new_string.split()
 print(x)
 
-
+reversed = ''.join(reversed(x))
+print(reversed)
 
 
 # print(x[::-1])
