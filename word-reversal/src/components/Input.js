6944410769b927eaec1916b663ca378b4757bea9
Laura Miller <lmiller.spence@gmail.com>
2020-05-12 17:41:14 +0100

    Changing input field and button text

--- word-reversal/src/components/Input.js
+++ word-reversal/src/components/Input.js
@@ -2,12 +2,22 @@ import React from 'react';
 
 class Input extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {text: '', result: 'Result'};
+      }
+
+    onClick(event) {
+        this.setState({result:this.state.text.split("").reverse().join("")})
+      }
+  
+
     render() {
         return (
         <div className="container addtask">
             <div className="row">
                 <div className="col-5 offset-4">
-                    <h1>Add your task to the list here: </h1>
+                    <h1>Enter text to be reversed here: </h1>
                 </div>
             </div>
             <div className="row">
@@ -16,12 +26,12 @@ class Input extends React.Component {
                         <input
                             type="text"
                             className="form-control"
-                            placeholder="Task to add...."
+                            placeholder="Enter text..."
                             onChange={this.taskDescriptionChanged}
                         />
                         <div className="input-group-append">
-                            <button className="btn addButton btn-outline-primary" type="button" onClick={this.addTask}>
-                                Add
+                            <button className="btn addButton btn-outline-primary" type="button" onClick={this.onClick.bind(this)}>
+                                Submit
                             </button>
                         </div>
                     </div>
