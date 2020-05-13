f56cf40ac21ed831c2d91ac9b420f91e4070c339
Laura Miller <lauramiller.spence@gmail.com>
2020-05-13 15:05:44 +0100

    Trying different tactic

--- reversal_function.py
+++ reversal_function.py
@@ -1,8 +1,6 @@
 string = input("Enter text to be reversed: " )
 
 def reverse_words(string):
-    string = (string[::-1])
-    string.split()
-    # reversed = ' '.join(reversed(x))
-    return
+    string = "".join(reversed(string))
+    return string
 
