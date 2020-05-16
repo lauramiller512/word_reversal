714266fb900b644fbb87ea8dd91c386fbd6b2e7f
Laura Miller <lauramiller.spence@gmail.com>
2020-05-16 17:51:25 +0100

    Playing with function

--- reversal_function.py
+++ reversal_function.py
@@ -1,7 +1,11 @@
 string = input("Enter text to be reversed: " )
 
+print (string)
+
 def reverse_words(string):
     string = "".join(reversed(string))
-    return string
+    x = string.split()
+    # string = (reversed(x)) 
+    return (string) 
 
-print (reverse_words(string))
+print (string)
